408e9d1ddbbfd2edb5206795f1ba80fffc35209f
Thiha <blabla31122000@gmail.com>
2022-01-18 13:56:16 +0630

    Up to LifeCycle Hooks

---
+++ src/components/counters.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    console.log("Counters - Rendered");
+
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm-2">
+          Reset
+        </button>
+        {counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
